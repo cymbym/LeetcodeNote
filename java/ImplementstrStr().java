25839c86cd708b6e9bf5d2211b425a7513ef55af
cymbym <670165595@qq.com>
2019-03-24 22:05:14 +0800

    update

--- java/ImplementstrStr().java
+++ java/ImplementstrStr().java
@@ -15,6 +15,8 @@ What should we return when needle is an empty string? This is a great question t
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 
+
+1.在String里用循环进行遍历
 1.在string里用循环进行遍历
 */
 class Solution {
