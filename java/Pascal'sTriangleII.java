1f2b2589f10fcb7cac57c2f837f70580b0b779a1
cymbym <670165595@qq.com>
2019-05-24 15:29:08 +0800

    update

--- java/Pascal'sTriangleII.java
+++ java/Pascal'sTriangleII.java
@@ -14,7 +14,8 @@ Follow up:
 
 Could you optimize your algorithm to use only O(k) extra space?
 
-1.因为要求空间复杂度为O(k)，故不能使用递归，只能用单层循环。要想只用单层循环，就得寻找该层各个数字的来源。由于int下，temp * (rowIndex - i + 1)会溢出，因此改用long，最后用(int)转化，而不是int()。
+1.因为要求空间复杂度为O(k)，故不能使用递归，只能用单层循环。要想只用单层循环，就得寻找该层各个数字的来源。
+2.由于int下，temp * (rowIndex - i + 1)会溢出，因此改用long，最后用(int)转化，而不是int()。
 */
 
 class Solution {
@@ -23,7 +24,7 @@ class Solution {
         long temp = 1;
         res.add(1);
         for(int i = 1; i < rowIndex; i ++ ){
-            temp = temp * (rowIndex - i + 1) / i;
+            temp = temp * (rowIndex - i + 1) / i;	
             res.add((int)temp);
         }
         if(rowIndex > 0) res.add(1);
