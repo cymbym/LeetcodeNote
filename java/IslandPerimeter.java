30e73a4b4bf347d7811a2fb5cd31760f2fadc881
cymbym <670165595@qq.com>
2019-07-18 11:27:46 +0800

    update

--- java/IslandPerimeter.java
+++ java/IslandPerimeter.java
@@ -19,7 +19,9 @@ Output: 16
 
 Explanation: The perimeter is the 16 yellow stripes in the image below:
 
-1.先统计方块个数，随后统计邻边数。所求 = 方块数 * 4 - 邻边数 * 2
+1.先统计方块个数，随后统计邻边数。所求 = 方块数 * 4 - 邻边数 * 2。
+2.我的解法：邻边数统计两次，最后除以2。7ms。
+3.最优解：邻边数其实只需统计一次，如左边和上边。
 
 */
 
@@ -30,31 +32,31 @@ class Solution {
         for (int i = 0; i < grid.length; i ++) {
             for (int j = 0; j < grid[0].length; j ++) {
                 if (grid[i][j] == 1) {
-                    count ++;
-                    if (i - 1 >= 0) {
-                        if (grid[i - 1][j] == 1) {
-                            redun ++;
-                        }
-                    }
-                    if (i + 1 < grid.length) {
-                        if (grid[i + 1][j] == 1) {
-                            redun ++;
-                        }
-                    }
-                    if (j - 1 >= 0) {
-                        if (grid[i][j - 1] == 1) {
-                            redun ++;
-                        }
-                    }
-                    if (j + 1 < grid[0].length) {
-                        if (grid[i][j + 1] == 1) {
-                            redun ++;
-                        }
-                    }
-                    
+                    count += 4;
+                    if (i - 1 >= 0 && grid[i - 1][j] == 1) redun ++;
+                    if (i + 1 < grid.length && grid[i + 1][j] == 1) redun ++;
+                    if (j - 1 >= 0 && grid[i][j - 1] == 1) redun ++;
+                    if (j + 1 < grid[0].length && grid[i][j + 1] == 1) redun ++;
                 }
             }
         }
-        return count * 4 - redun;
+        return count - redun;
+    }
+}
+
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int count = 0;
+        int redun = 0;
+        for (int i = 0; i < grid.length; i ++) {
+            for (int j = 0; j < grid[0].length; j ++) {
+                if (grid[i][j] == 1) {
+                    count += 4;
+                    if (i - 1 >= 0 && grid[i - 1][j] == 1) redun += 2;
+                    if (j - 1 >= 0 && grid[i][j - 1] == 1) redun += 2;
+                }
+            }
+        }
+        return count - redun;
     }
 }
