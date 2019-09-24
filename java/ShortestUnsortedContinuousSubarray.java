6454cd482f7277357759f5fd36393fefebcc470b
cymbym <670165595@qq.com>
2019-09-24 11:26:59 +0800

    update

--- java/ShortestUnsortedContinuousSubarray.java
+++ java/ShortestUnsortedContinuousSubarray.java
@@ -11,7 +11,11 @@ Note:
 Then length of the input array is in range [1, 10,000].
 The input array may contain duplicates, so ascending order here means <=.
 
-1.
+1.先在数组头遍历出第一个下降的值，随后开始以该值为基础，寻找最小值；
+2.同时在数组尾遍历出第一个上升的值，随后开始以该值为基础，寻找最大值，相当于确定了一个[min, max]区间；
+3.找区间里的数即可：在数组头寻找第一个大于这个最小值的数，在数组尾寻找第一个小于这个值的数。
+
+如[1,5,6,3,2,7,10,8,9,11]找出的区间为[2,10],对应的范围为5和9的位置。
 */
 
 
