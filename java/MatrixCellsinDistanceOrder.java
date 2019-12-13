a8a0a777141094cb04f6530f3e4eb4278ba7b5d9
cymbym <670165595@qq.com>
2019-12-13 17:29:24 +0800

    update

--- java/MatrixCellsinDistanceOrder.java
+++ java/MatrixCellsinDistanceOrder.java
@@ -40,6 +40,41 @@ Note:
 0 <= c0 < C
 
 
-
+1.我的解法：
 */
 
+class Solution {
+    ArrayList<int[]> arr = new ArrayList<int[]>();
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int r = r0;
+        int c = c0;
+        int cnt = 0;
+        addCell(R, C, r, c, new int[]{0, 0});
+        outerloop:  
+        while (cnt <= Math.max(R - r0 - 1, r0) + Math.max(C - c0 - 1, c0)) {
+            cnt ++;
+            int[][] vec0 = new int[][]{{0, cnt}, {0, -cnt}};
+            for (int[] k: vec0) addCell(R, C, r, c, k);
+            for (int i = 1; i < cnt; i ++) {
+                int j = cnt - i;
+                int[][] vec1 = new int[][]{{i, j}, {i, -j}, {-i, j}, {-i, -j}};
+                for (int[] k: vec1) addCell(R, C, r, c, k);
+            }
+            vec0 = new int[][]{{cnt, 0}, {-cnt, 0}};
+            for (int[] k: vec0) addCell(R, C, r, c, k);
+        }    
+        int[][] res = new int[arr.size()][2];
+        for (int i = 0; i < res.length; i ++) res[i] = arr.get(i);
+        return res;
+    }
+    public void addCell(int R, int C, int r, int c, int[] vector) {
+        r += vector[0];
+        c += vector[1];
+        if (r < R && r >= 0 && c < C && c >= 0) {
+            int[] tmp = new int[2];
+            tmp[0] = r;
+            tmp[1] = c;
+            arr.add(tmp);
+        }
+    }
+}
